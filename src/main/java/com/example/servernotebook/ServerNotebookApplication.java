0c0c0ce11b1fc434aeda587ee6a05586d2b59207
Fedosieienko Maksim <m.lisitsa2016@gmail.com>
2023-06-20 13:55:17 +0300

    first commit

---
+++ src/main/java/com/example/servernotebook/ServerNotebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.servernotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerNotebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerNotebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/servernotebook/ServerNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servernotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerNotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
